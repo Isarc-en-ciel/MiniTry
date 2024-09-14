1c0ff25c32d7f2125379da9d5386179c0fae7f24
Isabelle Waslet <iwaslet@mi-r4-p5.s19.be>
2024-09-14 17:28:43 +0200

    parsing

--- src/parsing/create_tokens.c
+++ src/parsing/create_tokens.c
@@ -6,7 +6,7 @@
 /*   By: iwaslet <iwaslet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 14:53:33 by iwaslet           #+#    #+#             */
-/*   Updated: 2024/09/14 15:58:22 by iwaslet          ###   ########.fr       */
+/*   Updated: 2024/09/14 17:28:09 by iwaslet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,33 +16,33 @@ t_lexer	create_pipe(char *input, int position, t_lexer token)
 {
     token.word = NULL;
     token.type = PIPE;
+    token.isthereaspace = 0;
     if (input[position - 1] && input[position - 1] == 32)
         token.isthereaspace = 1;
-    token.isthereaspace = 0;
 	return (token);
 }
 
 t_lexer create_redirect_in(char *input, int position, t_lexer token)
 {
 	token.word = NULL;
+    token.isthereaspace = 0;
 	if (input[position - 1] && input[position - 1] == 32)
         token.isthereaspace = 1;
-    token.isthereaspace = 0;
+	token.type = REDIR_IN;
 	if (input[position + 1] == '<')
 		token.type = REDIR_HEREDOC;
-	token.type = REDIR_IN;
 	return (token);
 }
 
 t_lexer create_redirect_out(char *input, int position, t_lexer token)
 {
 	token.word = NULL;
+    token.isthereaspace = 0;
 	if (input[position - 1] && input[position - 1] == 32)
         token.isthereaspace = 1;
-    token.isthereaspace = 0;
+	token.type = REDIR_OUT;
 	if (input[position + 1] == '>')
 		token.type = REDIR_APP;
-	token.type = REDIR_OUT;
 	return (token);
 }
 
