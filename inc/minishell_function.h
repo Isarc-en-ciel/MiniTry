6d7059db683e866fc331c017622946090aa91c40
iwaslet <iwaslet@student.42.fr>
2025-02-21 12:54:40 +0100

    Progress

--- inc/minishell_function.h
+++ inc/minishell_function.h
@@ -6,7 +6,7 @@
 /*   By: iwaslet <iwaslet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 16:38:18 by iwaslet           #+#    #+#             */
-/*   Updated: 2025/02/21 12:48:07 by iwaslet          ###   ########.fr       */
+/*   Updated: 2025/02/21 12:54:19 by iwaslet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,12 @@ struct sigaction	setup_signal_in_children(void);
 struct sigaction	setup_signal_in_parent(void);
 
 /*repo environment */
-char				**init_env(char **envp); //env_list.c
-t_env_list			*new_env_list(char *key, char *value, bool is_init); //env_list_func.c
-void				lst_addback(t_env_list **head, t_env_list *new); //env_list_func.c
+char				**init_env(char **envp);
+t_env_list			*new_env_list(char *key, char *value, bool is_init);
+void				lst_addback(t_env_list **head, t_env_list *new);
 void				delete_env(t_env_list *elem);
-int					get_list_size(t_env_list *head); //env_list_func.c
-void				destroy_lst(t_env_list **head); //env_list_func.c
+int					get_list_size(t_env_list *head);
+void				destroy_lst(t_env_list **head);
 t_env_list			*array_to_list(char **env);
 char				**list_to_array(t_env_list **head);
 void				ft_print_list(t_env_list *head);
@@ -56,8 +56,10 @@ bool				is_an_env_var(char *word, int i);
 t_shell				init_shell(char **envp);
 
 /* repo builtins */
-bool				init_env_list(t_env_list **head, t_command *cmd, char **env);
-int					builtin_error(t_command *cmd, char *str, enum e_error code, t_env_list **head);
+bool				init_env_list(t_env_list **head,
+						t_command *cmd, char **env);
+int					builtin_error(t_command *cmd, char *str,
+						enum e_error code, t_env_list **head);
 void				build_envp(t_env_list **head, t_command *cmd, char ***envp);
 bool				is_key_format(t_command *cmd, char *str);
 int					ft_cd(t_shell *sh, t_command *cmd);
@@ -70,25 +72,31 @@ int					ft_export(t_shell *sh, t_command *cmd);
 void				export_without_arg(t_env_list **head, t_command *cmd);
 
 /* repo execution */
-void				init_pipeline(t_shell *sh, int i, int pipe_fd[2], int prev_fd);
+void				init_pipeline(t_shell *sh, int i,
+						int pipe_fd[2], int prev_fd);
 void				init_child_pid(t_shell *sh);
 t_builtin			*find_builtin(t_shell *sh, t_command *cmd);
-void				perform_redirection(t_shell *shell, t_command *cmd); //file redirection.c
-void				create_heredoc(t_shell *shell, t_command *cmd, t_redirect *redirection);
-void				delete_heredoc_file(t_command *cmd);/* file exec_prompt.c */
-void				redirect_io(t_shell *shell, int fd_in, int fd_out); //file redirection.c
-void				exec_prompt(t_shell *shell); /* file exec_prompt.c */
-void				exec_external_cmd(t_shell *shell, int nb); /*file exec_command.c */
-void				exec_pipeline(t_shell *shell); //file src/execution/exec_pipeline.c
+void				perform_redirection(t_shell *shell, t_command *cmd);
+void				create_heredoc(t_shell *shell,
+						t_command *cmd, t_redirect *redirection);
+void				delete_heredoc_file(t_command *cmd);
+void				redirect_io(t_shell *shell, int fd_in, int fd_out);
+void				exec_prompt(t_shell *shell);
+void				exec_external_cmd(t_shell *shell, int nb);
+void				exec_pipeline(t_shell *shell);
 t_command			*pseudo_parsing(t_shell *shell, char *input);
 int					get_exit_status(t_command *cmd, pid_t pid);
-int					wait_children(t_shell *shell, pid_t *child_pid, int child_nb);
-int					configure_pipeline(t_shell *sh, int i, int pipe_fd[2], int prev_fd);
+int					wait_children(t_shell *shell,
+						pid_t *child_pid, int child_nb);
+int					configure_pipeline(t_shell *sh,
+						int i, int pipe_fd[2], int prev_fd);
 int					close_fd(int *p_fd);
-void				close_all_fds(int pipe_fd[2], int *prev_fd, int *in, int *out);
+void				close_all_fds(int pipe_fd[2],
+						int *prev_fd, int *in, int *out);
 char				*find_executable_path(t_shell *sh, int n, t_command *cmd);
 void				exit_child(t_shell *sh, int pipe_fd[2], int prev_fd, int i);
-void				exec_builtin(t_builtin *builtin, t_command *cmd, t_shell *sh);
+void				exec_builtin(t_builtin *builtin,
+						t_command *cmd, t_shell *sh);
 void				terminate_pipeline(t_shell *sh, int i, int prev_fd);
 t_shell				*clean_prompt(t_shell *sh);
 
@@ -105,13 +113,16 @@ void				exit_error(t_shell *shell, char *error);
 
 //tokenisation
 t_lexer				create_pipe(char *input, int *position, t_lexer token);
-t_lexer				create_redirect_in(char *input, int *position, t_lexer token);
-t_lexer				create_redirect_out(char *input, int *position, t_lexer token);
+t_lexer				create_redirect_in(char *input,
+						int *position, t_lexer token);
+t_lexer				create_redirect_out(char *input,
+						int *position, t_lexer token);
 t_lexer				create_doubleq(char *input, int *position, t_lexer token);
 t_lexer				create_simpleq(char *input, int *position, t_lexer token);
 t_lexer				create_word(char *input, int *position, t_lexer token);
 int					check_open_quotes(char *str, char c, int j);
-void				*ft_memcpy_plus(void *dst, const void *src, int start, int n);
+void				*ft_memcpy_plus(void *dst,
+						const void *src, int start, int n);
 
 //dynamic array
 t_darray			*retrieve_cmd(char *input, t_shell *shell);
@@ -129,17 +140,21 @@ void				ft_print_redir_type(enum e_tokens token);
 int					retrieve_pipe(char *input, int *i, int *j, t_darray *tab);
 int					retrieve_in(char *input, int *i, int *j, t_darray *tab);
 int					retrieve_out(char *input, int *i, int *j, t_darray *tab);
-int					retrieve_dquotes(char *input, int *i, int *j, t_darray *tab);
-int					retrieve_squotes(char *input, int *i, int *j, t_darray *tab);
+int					retrieve_dquotes(char *input, int *i,
+						int *j, t_darray *tab);
+int					retrieve_squotes(char *input, int *i,
+						int *j, t_darray *tab);
 int					retrieve_word(char *input, int *i, int *j, t_darray *tab);
 
 //parsing
 t_command			*parsing(char *input, t_shell *shell);
-t_stock				*parsing_starter(t_darray *tokens, t_stock	*tab, t_shell *shell);
+t_stock				*parsing_starter(t_darray *tokens,
+						t_stock	*tab, t_shell *shell);
 int					count_pipes(t_darray *tokens);
 t_stock				*error_return(t_darray *tokens, int i, t_shell *shell);
 int					into_cmds(int i, t_darray *tokens, t_stock *tab);
-int					fill_cmb_tab(int *j, int *l, t_darray *tokens, t_stock *tab);
+int					fill_cmb_tab(int *j, int *l,
+						t_darray *tokens, t_stock *tab);
 int					nbr_elem_cmd(int i, t_darray *tokens, t_stock *tab);
 int					check_grammar(t_stock *tab, int cmds);
 int					is_redirect(enum e_tokens token);
@@ -155,8 +170,10 @@ int					join_cmd(t_lexer *cmd, int size, int i, int a);
 int					ft_strcmp(const char *s, char n);
 char				*ft_strcpy(const char *s1, char *s2);
 t_stock				*tab_cleaner(t_stock *tab);
-int					clean_cmd(t_lexer *cmd, t_lexer **new_cmd, int nbr_elem, int *new_nbr_elem);
-int					count_new_elem(t_lexer *cmd, int nbr_elem, int *new_nbr_elem);
+int					clean_cmd(t_lexer *cmd, t_lexer **new_cmd,
+						int nbr_elem, int *new_nbr_elem);
+int					count_new_elem(t_lexer *cmd,
+						int nbr_elem, int *new_nbr_elem);
 int					copy_token(t_lexer cmd, t_lexer *new_cmd);
 void				new_empty_cmd(t_lexer *new_cmd);
 int					make_new_cmd(t_lexer *cmd, t_lexer **new_cmd, int nbr_elem);
